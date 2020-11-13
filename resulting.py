0c2633d9c455bdd7bccadea250628784bde4a7d0
TrojanDaniel <ds.young222@gmail.com>
2020-11-13 10:30:04 -0800

    Update resulting.py

--- resulting.py
+++ resulting.py
@@ -2,19 +2,16 @@
 import pandas as pd
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
+from bokeh.models import Div, DatePicker, ColumnDataSource, TableColumn, DataTable, HoverTool
 from bokeh.plotting import figure, curdoc
 from bokeh.layouts import column, row
 from bokeh.core.properties import value
 from bokeh.io import show
-from bokeh.models import Div, DatePicker, ColumnDataSource, TableColumn, DataTable, HoverTool
 
 
-cdph_race = "https://raw.githubusercontent.com/datadesk/california-coronavirus-data/master/cdph-race-ethnicity.csv"
 cdph_total = "https://raw.githubusercontent.com/datadesk/california-coronavirus-data/master/cdph-state-totals.csv"
 total_file = pd.read_csv(cdph_total)
 newest_update_date = total_file.iloc[1, 0]
-
-
 
 desc= Div(text="""All data are provided by Los Angeles Times.
 Repo Link:https://github.com/datadesk/california-coronavirus-data.
@@ -32,11 +29,11 @@ for i in range(len(total_file["date"])-1):
         if total_file["date"][i]==picked_date:
                 day_confirmed_cases=total_file["confirmed_cases"][i]-total_file["confirmed_cases"][i+1]
                 break
-data1={"date":[picked_date],"day_confirmed_cases":[day_confirmed_cases]}
-source1=ColumnDataSource(data1)
-col1=[TableColumn(field="date",title="DATE"),TableColumn(field="day_confirmed_cases",title="DAY CONFIRMED CASES")]
-dt1=DataTable(source=source1,columns=col1,width=500,height=200)
 
+data1=dict(date=[picked_date],day_confirmed_cases=[day_confirmed_cases])
+col1=[TableColumn(field="date",title="DATE"),TableColumn(field="day_confirmed_cases",title="DAY CONFIRMED CASES")]
+source1=ColumnDataSource(data1)
+dt1=DataTable(source=source1,columns=col1,width=500,height=200)
 ht1 = HoverTool(
 	tooltips=[
 		("date", "@date"),
@@ -58,17 +55,24 @@ desc2 = Div(text = """For a particular day, <b> 1. the %percent cases by race co
 general population  2. the %percent deaths by race compared to their representation in the
 general population 3.population percent</b> are shown in the figure""")
 
-
+cdph_race = "https://raw.githubusercontent.com/datadesk/california-coronavirus-data/master/cdph-race-ethnicity.csv"
 race_file = pd.read_csv(cdph_race)
-table_race = race_file[(race_file["age"] == "all") & (race_file["date"] == picked_date)]
-race_cols = ["date","race","confirmed_cases_percent","deaths_percent","population_percent"]
-source2= ColumnDataSource(data = table_race[race_cols])
 
-cols2 = [TableColumn(field="date", title="DATE"),TableColumn(field="race", title="RACE"),
+table_race = race_file[(race_file["age"] == "all") & (race_file["date"] == picked_date)]
+race_types=table_race.race.tolist()
+race_cols=list(table_race) # all column name
+race_cols.remove('date')
+race_cols.remove('age')
+race_cols.remove('deaths_total')
+
+cols2 = [TableColumn(field="race", title="RACE"),
     TableColumn(field="confirmed_cases_percent", title="CONFIRMED CASES PERCENT")
     ,TableColumn(field="deaths_percent", title="DEATHS PERCENT"),TableColumn(field="population_percent", title="POPULATION PERCENT")]
 
-dt_race = DataTable(source = source2, columns=cols2, width=600, height=200)
+
+source2= ColumnDataSource(data = table_race[race_cols])
+
+# dt_race = DataTable(source = source2, columns=cols2, width=600, height=200)
 
 
 ht2 = HoverTool(
@@ -77,8 +81,10 @@ ht2 = HoverTool(
 colors=["#DB7093","#6A5ACD","#3CB371"]
 new_cols= ["confirmed_cases_percent","deaths_percent","population_percent"]
 p2 = figure(x_range=table_race[race_cols]["race"],plot_width=500, plot_height=500,title="",tools=[ht2])
+
+
 p2.vbar_stack(new_cols,x='race',source=source2,
-                         width=0.3, color=colors,name=new_cols, legend=[value(x) for x in new_cols])
+                         width=0.3, color=colors,legend_label=["%s" % x for x in new_cols])
 p2.legend.orientation = "vertical"
 p2.legend.location = "top_left"
 
@@ -93,12 +99,15 @@ def call_back(attr, old, new):
         if total_file["date"][i] == picked_date:
             day_confirmed_cases = total_file["confirmed_cases"][i] - total_file["confirmed_cases"][i + 1]
             break
-    data1 = {"date": [picked_date], "day_confirmed_cases": [day_confirmed_cases]}
+    data1=dict(data=[picked_date],day_confirmed_cases=[day_confirmed_cases])
     source1.data= data1
 
-
-    table_race = race_file[(race_file["age"] == "all") & (race_file["date"] == picked_date)]
-    race_cols = ["date", "race", "confirmed_cases_percent", "deaths_percent", "population_percent"]
+    #quesition2
+    table_race =race_file[(race_file["age"] == "all") & (race_file["date"] == picked_date)]
+    race_cols = list(table_race)  # all column name
+    race_cols.remove('date')
+    race_cols.remove('age')
+    race_cols.remove('deaths_total')
     race_data = table_race[race_cols]
     source2.data=race_data
 
